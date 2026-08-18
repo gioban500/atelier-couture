5596245a795ec47c09e248f4522e31d46068a195
gioban500 <BANEZIgiovanny@gmail.com>
2026-08-18 11:00:06 +0000

    email aain

--- couturier-backend/server.js
+++ couturier-backend/server.js
@@ -11,9 +11,10 @@ import devisRoutes from './routes/devis.js';
 import portfolioRoutes from './routes/portfolio.js';
 import adminRoutes from './routes/admin.js';
 
+const app = express();
+
 app.set('trust proxy', 1);
 
-const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware CORS
